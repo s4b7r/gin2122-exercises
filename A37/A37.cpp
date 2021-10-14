3ed45cfd8b9b097cf1d2245676446ea86ec7f63f
s4b7r <github@s4b7r.de>
2021-10-14 18:27:02 +0200

    Add 56 to A37

--- A37/A37.cpp
+++ A37/A37.cpp
@@ -2,8 +2,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <float.h>
+
+struct cylinder_dimensions {
+    double radius;
+    double height;
+};
 
 int divide(int x, int y) {
     return x / y;
@@ -11,19 +17,28 @@ int divide(int x, int y) {
 int multiply(int x, int y) {
     return x * y;
 }
-
+int cylinder_volume(struct cylinder_dimensions cydim) {
+    return (int)(M_PI * cydim.radius*cydim.radius*cydim.height);
+}
 void binary_operators() {
+    printf("1 x*y, 2 x/y\n");
+    printf("3 cylinder volume: x height, y radius\n");
+    int choice;
+    scanf("%d", &choice);
     int x, y;
     printf("x: ");
     scanf("%d", &x);
     printf("y: ");
     scanf("%d", &y);
-    int choice;
-    printf("1*, 2/\n");
-    scanf("%d", &choice);
     int result;
     if (choice == 1) result = multiply(x, y);
     else if (choice == 2) result = divide(x, y);
+    else if (choice == 3) {
+        struct cylinder_dimensions dim_input;
+        dim_input.radius = y;
+        dim_input.height = x;
+        result = cylinder_volume(dim_input);
+    }
     printf("%d\n", result);
 
 }
@@ -138,11 +153,11 @@ void multiply_by_bitshift() {
     int input;
     printf("number: ");
     scanf("%d", &input);
-    printf("%d * 4 = %d", input, input << 2);
+    printf("%d * 4 = %d\n", input, input << 2);
 }
 int main(int argc, char **argv) {
     int choice;
-    printf("1*/\n");
+    printf("1 binary operations\n");
     printf("2 cont.multiplication\n");
     printf("3 mean\n");
     printf("4 sin\n");
